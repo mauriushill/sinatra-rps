7ef957bec862126e30a0e3493df698f4f607e7e2
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2023-11-20 22:38:01 +0000

    Almost there

--- app.rb
+++ app.rb
@@ -2,8 +2,55 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb( :homepage)
+
 end
+
+
+get("/rock") do
+moves = ["rock", "paper", "scissors"]
+
+@compu_move = moves.sample
+
+if @compu_move == "rock"
+  @outcome = "tied"
+elsif @compu_move == "scissors"
+@outcome = "won"
+else 
+  @outcome = "lost"
+end
+
+ erb( :lion)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  
+  @compu_move = moves.sample
+  
+  if @compu_move == "paper"
+    @outcome = "tied"
+  elsif @compu_move == "rock"
+  @outcome = "won"
+  else 
+    @outcome = "lost"
+  end
+  
+   erb( :monkey)
+  end
+
+  get("/scissors") do
+    moves = ["rock", "paper", "scissors"]
+    
+    @compu_move = moves.sample
+    
+    if @compu_move == "scissors"
+      @outcome = "tied"
+    elsif @compu_move == "paper"
+    @outcome = "won"
+    else 
+      @outcome = "lost"
+    end
+    
+     erb( :tiger)
+    end
